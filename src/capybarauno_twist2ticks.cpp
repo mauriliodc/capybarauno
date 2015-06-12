8341c10b5f3bb3b9f68e642a7aaf06c00d14a347
mauriliodc <maurilio.dicicco@gmail.com>
2015-06-12 10:14:18 +0200

    added baseline parameter

--- src/capybarauno_twist2ticks.cpp
+++ src/capybarauno_twist2ticks.cpp
@@ -15,24 +15,26 @@ ros::Publisher ticks_publisher;
 struct configuration{
     string cmdvel_topic;
     string ticks_topic;
-    int left_meter_to_ticks;
-    int right_meter_to_ticks;
+    double left_ticks;
+    double right_ticks;
+    double baseline;
     int debug;
 };
 
 struct configuration c;
 uint32_t tick_sequence;
 
-float left_meter_to_ticks,right_meter_to_ticks;
+
+float left_meter_to_ticks,right_meter_to_ticks,baseline;
 
 void cmdvelCallback(const geometry_msgs::Twist::ConstPtr& twist)
 {
     if(!ros::ok()) return;
     //get absolute speed values, expressed in tick per interval
     float translational_velocity = twist->linear.x;
-    float rotational_velocity    = twist->angular.z;
+    float rotational_velocity    = twist->angular.z/baseline;
     if(c.debug){
-        ROS_INFO("LINEAR %f ANGULAR %f", twist->linear.x, twist->angular.z);
+        ROS_INFO("LINEAR %f ANGULAR %f ANGULAR AFTER BASELINE %f", twist->linear.x, twist->angular.z,rotational_velocity);
     }
     capybarauno::capybara_ticks ct;
     ct.leftEncoder=(-translational_velocity+rotational_velocity)*left_meter_to_ticks;
@@ -51,8 +53,9 @@ void cmdvelCallback(const geometry_msgs::Twist::ConstPtr& twist)
 void EchoParameters(){
     printf("%s %s\n","_cmdvel_topic",c.cmdvel_topic.c_str());
     printf("%s %s\n","_ticks_topic",c.ticks_topic.c_str());
-    printf("%s %d\n","_left_meter_to_ticks",c.left_meter_to_ticks);
-    printf("%s %d\n","_right_meter_to_ticks",c.right_meter_to_ticks);
+    printf("%s %f\n","_left_ticks",c.left_ticks);
+    printf("%s %f\n","_right_ticks",c.right_ticks);
+    printf("%s %f\n","_baseline",c.baseline);
     printf("%s %d\n","_debug",c.debug);
 }
 
@@ -64,13 +67,15 @@ int main(int argc, char **argv)
     ros::NodeHandle n("~");
     n.param<string>("cmdvel_topic", c.cmdvel_topic, "/cmd_vel");
     n.param<string>("tick_topic", c.ticks_topic, "/requested_ticks");
-    n.param("left_meter_to_ticks",c.left_meter_to_ticks,1);
-    n.param("right_meter_to_ticks",c.right_meter_to_ticks,1);
+    n.param("left_ticks",c.left_ticks,1.0d);
+    n.param("right_ticks",c.right_ticks,1.0d);
+    n.param("baseline",c.baseline,1.0d);
     n.param("debug", c.debug, 1);
 
     EchoParameters();
-    left_meter_to_ticks = (float)c.left_meter_to_ticks;
-    right_meter_to_ticks = (float)c.right_meter_to_ticks;
+    left_meter_to_ticks = (float)c.left_ticks;
+    right_meter_to_ticks = (float)c.right_ticks;
+    baseline= (float)c.baseline;
 
     ros::Subscriber cmdvel_subscriber = n.subscribe(c.cmdvel_topic.c_str(), 1000, cmdvelCallback);
     ticks_publisher = n.advertise<capybarauno::capybara_ticks>(c.ticks_topic.c_str(), 1000);
